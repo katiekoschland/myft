6f93d73fd168bf3289f2302f4f32c91b3158f1be
katiekoschland <katie.koschland@ft.com>
2017-10-12 12:15:40 +0100

    BBC Headline API setup

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const request = require('request');
+const bbcApiKey = '537b165a4f314fedae8cb39788d4d713';
+// const bbcApi = require('.yy/bbc-api')
 // const ftApi = require('./headlines.js');
-const isomorphicFetch = require('isomorphic-fetch');
 
 const app = express();
 
@@ -11,7 +12,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let handlebars = exphbs.create({
-  defaultLayout: 'main',
   extname: '.html'
 });
 
@@ -34,7 +34,7 @@ app.post('/', (req,res) => {
   'x-api-key': 'a7anxw8mdqq33pfnavcaq66s' },
   body:
   `{"queryString": "${query}",
-  "resultContext" : {"aspects" :["title"]}}`
+  "resultContext" : {"aspects" :["title","lifecycle","location","summary","editorial" ]}}`
 };
 
 request(options, (error, response, body) => {
@@ -46,10 +46,30 @@ request(options, (error, response, body) => {
 });
 });
 
+app.get('/bbc', (req, res) => {
+  console.log('hello')
+  res.render('../views/layouts/bbc.html');
+});
+
+app.post('/bbc', (req, res) => {
+  const queryBBC = req.body.queryBBC;
+  const optionsBBC = { method: 'GET',
+  url: `https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=${bbcApiKey}`
+};
+
+request(optionsBBC, (error, response, body) => {
+  if (error) throw new Error(error);
+  const queryBBC = JSON.parse(body);
+  console.log(queryBBC)
+  res.render('../views/layouts/bbc.html');
+});
+});
+
 const PORT = process.env.PORT || 5000;
 
 const listen = app.listen(PORT, () => {
   console.log('Listening on %s', PORT);
 });
+
 
 module.exports = listen;
--- server/headlines.js
+++ server/headlines.js
@@ -1,25 +1,25 @@
-const isomorphicFetch = require('isomorphic-fetch');
-
-const query = req.body.query;
-
-const body = JSON.stringify({
-  'queryString': `${query}`,
-  'resultContext' : {
-    'aspects' :[ 'title','lifecycle','location','summary','editorial' ],
-  }
-});
-
-fetch('//api.ft.com/content/search/v1', {
-  method: 'POST',
-  body: body,
-  headers: {
-    'x-api-key': 'a7anxw8mdqq33pfnavcaq66s',
-    'content-type': 'application/javascript'
-  }
-}).then(response => {
-  const res = JSON.parse(response);
-  console.log(res);
-})
-.catch(e => {
-  throw e;
-});
+// const isomorphicFetch = require('isomorphic-fetch');
+//
+// const query = req.body.query;
+//
+// const body = JSON.stringify({
+//   'queryString': `${query}`,
+//   'resultContext' : {
+//     'aspects' :[ 'title','lifecycle','location','summary','editorial' ],
+//   }
+// });
+//
+// fetch('//api.ft.com/content/search/v1', {
+//   method: 'POST',
+//   body: body,
+//   headers: {
+//     'x-api-key': 'a7anxw8mdqq33pfnavcaq66s',
+//     'content-type': 'application/javascript'
+//   }
+// }).then(response => {
+//   const res = JSON.parse(response);
+//   console.log(res);
+// })
+// .catch(e => {
+//   throw e;
+// });
